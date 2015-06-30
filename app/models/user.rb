e84ea81836e1499dd7e5062996dd2ce9207c49f3
Rodney Cullen <rodcul@gmail.com>
2015-06-30 16:48:31 +0100

    Removed byebug from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,3 @@
-require 'byebug'
-
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -8,7 +6,6 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      # byebug
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name   # assuming the user model has a name
