379cc5eede9c667249c25eb4bf91d0bc578e42c2
Rodney Cullen <rodcul@gmail.com>
2015-07-01 00:01:44 +0100

    Omniauth rspec passing!

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,11 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :omniauthable, :omniauth_providers => [:facebook]
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
+    where(provider: auth["provider"], uid: auth["uid"]).first_or_create do |user|
+      user.email = auth["info"]["email"] unless auth["info"]["email"].blank?
       user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.name   # assuming the user model has a name
-      user.image = auth.info.image # assuming the user model has an image
+      user.name = auth["info"]["name"] unless auth["info"]["name"].blank?  # assuming the user model has a name
+      user.image = auth["info"]["image"] unless auth["info"]["image"].blank? # assuming the user model has an image
     end
   end
 
--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -1,43 +1,43 @@
 require 'rails_helper'
+require 'byebug'
 
-feature "user can sign in and sign out" do
-
-  context "user not signed in and visits homepage" do
-    it "should see 'sign in', 'sign out'" do
+feature 'user can sign in and sign out' do
+  context 'user not signed in and visits homepage' do
+    it 'should see Sign in with Facebook' do
       visit '/'
-      expect(page).to have_link('Sign in')
-      expect(page).to have_link('Sign up')
+      expect(page).to have_link('Sign in with Facebook')
     end
 
     it "shoudln\'t see a sign out link" do
       visit '/'
       expect(page).not_to have_link('Sign out')
     end
-
   end
 
-  context "user is signed in and on the homepage" do
-
+  context 'user is signed in and on the homepage' do
     before do
-      visit('/')
-      click_link('Sign up')
-      fill_in('Email', with: 'test@test.es')
-      fill_in('Password', with: 'pAssw0rd')
-      fill_in('Password confirmation', with: 'pAssw0rd')
-      click_button('Sign up')
+      OmniAuth.config.test_mode = true
+      OmniAuth.config.mock_auth[:facebook] = {
+        'provider'  => 'facebook',
+        'uid'       => 12345,
+        'info' => {
+          'email' => 'email@email.com',
+          'name'       => 'John Doe',
+          'image'       => 'http://robohash.org/treemo'
+          # any other attributes you want to stub out for testing
+        }
+      }
+      visit root_path
     end
 
-    it "should see signout link" do
-      visit ('/')
+    it 'should see signout link' do
+      click_link 'Sign in with Facebook'
       expect(page).to have_link('Sign out')
     end
 
     it "shouldn't see signup or signin link" do
-      visit('/')
-      expect(page).not_to have_link('Sign in')
-      expect(page).not_to have_link('Sign up')
+      click_link 'Sign in with Facebook'
+      expect(page).not_to have_link('Sign in with Facebook')
     end
-
   end
-
 end
