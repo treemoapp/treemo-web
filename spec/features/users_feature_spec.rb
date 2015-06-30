549012e9638aea8624e371ca97739dd16678e1b2
Rodney Cullen <rodcul@gmail.com>
2015-07-01 00:15:12 +0100

    Removed byebug and used visit root_path

--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
-require 'byebug'
 
 feature 'user can sign in and sign out' do
   context 'user not signed in and visits homepage' do
     it 'should see Sign in with Facebook' do
-      visit '/'
+      visit root_path
       expect(page).to have_link('Sign in with Facebook')
     end
 
