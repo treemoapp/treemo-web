3f857f766c3d64cd87655c03845cbb3abce927a6
Rodney Cullen <rodcul@gmail.com>
2015-07-02 11:08:59 +0100

    Changed faker to use 16 numbers for fb id

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,5 +9,5 @@ require 'faker'
   user.locations.create(name: Faker::Company.name,
                         lat: Faker::Address.latitude,
                         lng: Faker::Address.longitude,
-                        facebook_id: Faker::Number.number(10))
+                        facebook_id: Faker::Number.number(16))
 end
