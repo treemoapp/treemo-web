8960d3e892daaad5af29cef3bfdf110d3e087866
Rodney Cullen <rodcul@gmail.com>
2015-07-06 11:44:32 +0100

    Moved from controller to application.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  config.middleware.insert_before 0, 'Rack::Cors' do
-    allow do
-      origins '*'
-      resource '*', headers: :any, methods: [:get, :post, :options]
-    end
-  end
 end
