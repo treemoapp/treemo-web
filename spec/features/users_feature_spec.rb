8821593b7e6bff9572adfe00fb0f69c6f97bf116
Dan Bolger <bowedcymbal@gmail.com>
2015-07-06 16:46:17 +0100

    adds failing test for showing stats

--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'byebug'
 
 feature 'user can sign in and sign out' do
   context 'user not signed in and visits homepage' do
@@ -40,6 +41,35 @@ feature 'user can sign in and sign out' do
     it "shouldn't see signup or signin link" do
       click_link 'Sign in with Facebook'
       expect(page).not_to have_link('Sign in with Facebook')
+    end
+  end
+end
+
+feature 'user can see how many checkins have been made at their locations' do
+    context 'user is signed in and on the homepage' do
+      before do
+      OmniAuth.config.test_mode = true
+      OmniAuth.config.mock_auth[:facebook] = {
+        'provider'  => 'facebook',
+        'uid'       => 12345,
+        'info' => {
+        'email' => 'email@email.com',
+        'name'       => 'John Doe',
+        'image'       => 'http://robohash.org/treemo'
+        # any other attributes you want to stub out for testing
+        }
+      }
+      visit root_path
+      click_link 'Sign in with Facebook'
+      20.times do |i|
+        User.first.locations.create
+        Checkin.create(user: User.first, location: Location.first)
+      end
+    end
+      it 'can click through to a page showing number of checkins' do
+      visit '/'
+      click_link 'View your profile'
+      expect(page).to have_content "20 people have checked in"
     end
   end
 end
