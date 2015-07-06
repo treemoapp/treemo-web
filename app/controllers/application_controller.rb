04e18d535df7fd95c8c580914b54c3e10bff4a45
Rodney Cullen <rodcul@gmail.com>
2015-07-06 11:39:15 +0100

    Revert to default application.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
+  config.middleware.insert_before 0, 'Rack::Cors' do
     allow do
       origins '*'
       resource '*', headers: :any, methods: [:get, :post, :options]
